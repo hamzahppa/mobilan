13bb2561842fd33720bc5618ab7bcca31c421e7e
hamzahppa <hamzahibnabdullah@gmail.com>
2017-03-22 11:13:21 +0700

    minor add index

--- section/section-contact.php
+++ section/section-contact.php
@@ -3,19 +3,25 @@
     <h3 class="text-xs-center m-b-3">Mulai beriklan<small class="text-muted"><br>hubungi kami sekarang juga, tim kami akan segera menghubungi anda segera</small></h3>
     <form>
       <div class="row">
-        <div class="col-md-6 col-xl-5">
+        <div class="col-md-6 col-xl-4">
           <div class="form-group has-icon-left form-control-name">
             <label class="sr-only" for="inputName">Nama Perusahaan</label>
             <input type="text" class="form-control form-control-lg" id="inputName" placeholder="Nama Perusahaan">
           </div>
         </div>
-        <div class="col-md-6 col-xl-4">
+        <div class="col-md-6 col-xl-3">
           <div class="form-group has-icon-left form-control-email">
             <label class="sr-only" for="inputEmail">Kontak</label>
             <input type="text" class="form-control form-control-lg" id="inputEmail" placeholder="Telepon/Email" autocomplete="off">
           </div>
         </div>
         <div class="col-md-6 col-xl-3">
+          <div class="form-group has-icon-left form-control-name">
+            <label class="sr-only" for="inputName">Paket</label>
+            <input type="text" class="form-control form-control-lg" id="inputName" placeholder="Paket">
+          </div>
+        </div>
+        <div class="col-md-6 col-xl-2">
           <div class="form-group">
             <button type="submit" class="btn btn-success btn-block">Kirim</button>
           </div>
